1fda85cc3813833b80b6d2fb4f79da064e65115f
Harsh Rajbhar <hkrajbhar2002@gmail.com>
2022-08-18 15:09:41 +0530

    navbar design completed with responsive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NavbarComponent {
+  activeindex = 0;
+  isHamActive = false;
+  navlist: any[] = [
+    {
+      name: 'Home',
+      url: '',
+      index: 0,
+    },
+    {
+      name: 'About',
+      url: 'about',
+      index: 1,
+    },
+    {
+      name: 'Service',
+      url: 'service',
+      index: 2,
+    },
+    {
+      name: 'Work',
+      url: 'work',
+      index: 3,
+    },
+    {
+      name: 'Contact',
+      url: 'contact',
+      index: 4,
+    },
+  ];
 }
