8769ff010488f7071acb4c3afce01c13a20c5dc0
Keyur Shah <rpsoftechindia@gmail.com>
2022-09-02 12:29:14 +0530

    .

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
   exports: [ContactComponent],
 })
 export class ContactModule {}
--- src/app/contact/contact/contact.component.ts
+++ src/app/contact/contact/contact.component.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+import {
+  JSONObjectKeyAndTypeValidator,
+  ValidateJSONObject,
+} from 'rps-jsonvalidator';
+
+// const nodemailer = require('nodemailer');
+// import * as nodemailer from 'nodemailer'
+
+interface contactUsInterface {
+  your_name: string;
+  your_email: string;
+  your_no: string;
+  your_msg: string;
+}
 
 @Component({
   selector: 'app-contact',
@@ -6,15 +20,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  validationForNumber(event: KeyboardEvent) {
-    const isNumber = event.key;
-    console.log(isNumber);
+  error = '';
 
-    if (isNumber === 'Backspace') {
-      return;
-    }
-    if (typeof isNumber !== 'undefined' && isNaN(+isNumber)) {
-      event.preventDefault();
+  // getdata() {
+  //
+  // }
+
+  contact_us_details: contactUsInterface = {
+    your_name: '',
+    your_email: '',
+    your_no: '',
+    your_msg: '',
+  };
+  ContactValidatorObj: JSONObjectKeyAndTypeValidator[] = [
+    {
+      key: 'your_name',
+      required: true,
+      type: 'string',
+      regex: /^[a-zA-Z ]+$/,
+      error: '* Enter Your name',
+      lengths: {
+        min_len: 1,
+      },
+    },
+    {
+      key: 'your_email',
+      required: true,
+      type: 'string',
+      regex: /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/,
+      error: '* Enter valid email',
+    },
+    {
+      key: 'your_no',
+      required: true,
+      type: 'string',
+      error: '* Enter valid phone number',
+      regex: /^[a-zA-Z0-9+_.-]+$/,
+      lengths: {
+        len: 10,
+      },
+    },
+
+    {
+      key: 'your_msg',
+      required: true,
+      type: 'string',
+      lengths: {
+        min_len: 1,
+      },
+      error: '* Enter Message',
+    },
+  ];
+
+  getdata() {
+    try {
+      ValidateJSONObject(this.contact_us_details, this.ContactValidatorObj);
+      console.log(this.contact_us_details);
+      // this.emailmethod();
+      this.error = '';
+    } catch (error: any) {
+      this.error = error;
     }
   }
+  // emailmethod() {
+  //   let mailTransporter = nodemailer.createTransport({
+  //     service: 'gmail',
+  //     auth: {
+  //       user: 'rajbharharsh403@gmail.com',
+  //       pass: '8866128435Harsh',
+  //     },
+  //   });
+
+    // let details = {
+    //   from: this.contact_us_details.your_email,
+    //   to: 'rajbharharsh403@gmail.com',
+    //   subject: 'testing porpose',
+    //   text: 'test our page',
+    // };
+
+  //   mailTransporter.sendMail(details, (err: any) => {
+  //     if (err) {
+  //       console.log('error');
+  //     } else {
+  //       console.log('sent');
+  //     }
+  //   });
+  // }
 }
