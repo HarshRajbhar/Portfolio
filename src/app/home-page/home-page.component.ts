868433ab72ff66151de5f4c0e69673302eb7ed3f
Harsh Rajbhar <91482495+HarshRajbhar@users.noreply.github.com>
2023-02-07 11:10:54 +0530

    Seo issue Solved (#4)
    * Fragement issue Solved
    
    * Robot.txt & sitemap.xml file added

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,18 +1,33 @@
 import { NgFor, isPlatformServer } from '@angular/common';
-import { Component, Inject, PLATFORM_ID, Type } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Inject,
+  OnDestroy,
+  PLATFORM_ID,
+  Type,
+} from '@angular/core';
 import { ContactComponent } from '../contact/contact/contact.component';
 import { AboutUsComponent } from '../about-us/about-us/about-us.component';
 import { ServiceComponent } from '../services/service/service.component';
 import { WorkComponent } from '../work/work/work.component';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription, firstValueFrom, retry } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
-  standalone:true,
-  imports:[ContactComponent,AboutUsComponent,ServiceComponent,WorkComponent,NgFor],
+  standalone: true,
+  imports: [
+    ContactComponent,
+    AboutUsComponent,
+    ServiceComponent,
+    WorkComponent,
+    NgFor,
+  ],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit,OnDestroy{
   activeImageIndex = 0;
 
   testimonial: any[] = [
@@ -35,7 +50,9 @@ export class HomePageComponent {
     },
   ];
 
-  constructor(@Inject(PLATFORM_ID) id: any) {
+  HeaderHeight = 180;
+  subscription!:Subscription;
+  constructor(@Inject(PLATFORM_ID) id: any, private activated: ActivatedRoute) {
     if (isPlatformServer(id)) {
       return;
     }
@@ -46,6 +63,48 @@ export class HomePageComponent {
       }
     }, 4000);
   }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+  ngAfterViewInit(): void {
+    this.subscription=  this.activated.params.subscribe(({frag})=>{
+      console.log(frag);
+      setTimeout(()=>{
+        this.scrollToFrag(frag);
+      })
+     
+    })
+  }
+  private scrollToFrag(frag:string){
+    if (typeof frag !== 'undefined') {
+      firstValueFrom(
+        new Observable((obs) => {
+          const ele = document.getElementById(frag);
+          if (ele === null) {
+            obs.error('Error');
+          } else {
+            obs.next('a');
+            const bou = ele.getBoundingClientRect();
+
+            setTimeout(() => {
+              document.scrollingElement?.scrollBy(
+                bou.x,
+                bou.y - this.HeaderHeight / 2
+              );
+            });
+
+            obs.complete();
+          }
+        }).pipe(
+          retry({
+            count: 7,
+            delay: 1000,
+          })
+        )
+      );
+    }
+  }
+
   previousindex() {
     if (this.activeImageIndex > 0) {
       this.activeImageIndex--;
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -17,6 +17,7 @@ export class NavbarComponent {
       name: 'Home',
       url: '',
       index: 0,
+      fragment: '',
     },
     {
       name: 'About',
